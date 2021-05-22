3b2e3a0c849d6109e8f77fabc1da273a25d1641a
Yassine Bridi <ybridi@gmail.com>
2021-05-22 22:56:34 +0100

    Init

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash/
+###########################################################################
+### Bash script to install my prefered packages on a new ubuntu server. ###
+### Yassine Bridi https://yasbr.com                                     ###
+### Args: - $1: email, for SSH key generation                           ###
+###
+###########################################################################
+
+echo '### Updating ubuntu repos'
+sudo apt update -y
+
+echo '### Installing Zsh..'
+sudo apt install zsh -y
+
+echo '### Installing Oh My Zsh..'
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo '### Installing Zsh plugins'
+
+echo '### zsh-autosuggestions'
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo '### zsh-syntax-highlighting'
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo '### zsh-completions'
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+echo '### Adding Zsh config'
+wget -O /root/.zshrc  https://raw.githubusercontent.com/yassinebridi/server-setup/master/zsh/.zshrc
+
+echo '### Installing Neovim..'
+sudo apt install neovim -y
+
+echo '### Adding Neovim config'
+mkdir -p /root/.config/nvim
+wget -O /root/.config/nvim/init.vim https://raw.githubusercontent.com/yassinebridi/server-setup/master/vim
+
+echo '### Installing fnm, and installing the latest LTS NodeJS with it'
+curl -fsSL https://fnm.vercel.app/install | bash
+fnm install
+
+echo "Installing exa, using a prebuilt binary, because it's the only way it would work"
+wget -O /usr/bin/exa https://0x0.st/-B6R.so
+chmod +x /usr/bin/exa
+
+echo 'Generating a new ssh key'
+ssh-keygen -t ed25519 -C "$1"
---
+++ zsh/.zshrc
@@ -0,0 +1,44 @@
+# path is: /root/.zshrc
+
+export ZSH="/root/.oh-my-zsh"
+ZSH_THEME="crunch"
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)
+
+source $ZSH/oh-my-zsh.sh
+
+export TERM=screen-256color
+
+# Use vim keys in tab complete menu:
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M viins 'jk' vi-cmd-mode
+bindkey -M vicmd 'gl' vi-end-of-line
+bindkey -M vicmd 'gh' vi-digit-or-beginning-of-line
+bindkey -v '^?' backward-delete-char
+
+# Aliases
+alias v='nvim'
+alias dk='dokku'
+alias less='batcat'
+alias ld='lazydocker'
+alias svim='sudo -E nvim'
+alias dvim='nvim -d'
+alias zc='v /root/.zshrc'
+alias hg='history | grep'
+
+alias tr='cd /home/dokku/tour-api'
+alias rc='docker exec -it tour-api.web.1'
+alias resetpg="for command in 'drop database metrics;' 'create database metrics;'; do docker exec -it $(docker ps | grep postgres | awk '{print $1}') psql -U postgres -d postgres -c $command; done"
+
+alias la='exa -la --group-directories-first'
+alias ls='exa -l --group-directories-first'
+
+# fnm
+export PATH=/root/.fnm:$PATH
+eval "`fnm env`"
+
+# fnm
+export PATH=/root/.fnm:$PATH
+eval "`fnm env`"
