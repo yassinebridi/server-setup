6ce7c7b16132544d213c22666e6f40f8c0c943e2
Yassine Bridi <ybridi@gmail.com>
2021-05-22 23:19:11 +0100

    Script colors

--- install.sh
+++ install.sh
@@ -5,46 +5,53 @@
 ### Args: - $1: email, for SSH key generation                           ###
 ###########################################################################
 
-echo '### Updating ubuntu repos'
+# Colors
+RED="31"
+GREEN="32"
+BOLDGREEN="\e[1;${GREEN}m"
+ITALICRED="\e[3;${RED}m"
+ENDCOLOR="\e[0m"
+
+echo -e "${BOLDGREEN}### Updating ubuntu repos${NOCOLOR}"
 sudo apt update -y
 
-echo '### Installing Zsh..'
+echo -e "${BOLDGREEN}### Installing Zsh..${NOCOLOR}"
 sudo apt install zsh -y
 
-echo '### Installing Oh My Zsh..'
+echo -e "${BOLDGREEN}### Installing Oh My Zsh..${NOCOLOR}"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo '### Installing Zsh plugins'
+echo -e "${BOLDGREEN}### Installing Zsh plugins${NOCOLOR}"
 
-echo '### zsh-autosuggestions'
+echo -e "${BOLDGREEN}### zsh-autosuggestions${NOCOLOR}"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo '### zsh-syntax-highlighting'
+echo -e "${BOLDGREEN}### zsh-syntax-highlighting${NOCOLOR}"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-echo '### zsh-completions'
+echo -e "${BOLDGREEN}### zsh-completions${NOCOLOR}"
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
-echo '### Adding Zsh config'
+echo -e "${BOLDGREEN}### Adding Zsh config${NOCOLOR}"
 wget -O /root/.zshrc https://raw.githubusercontent.com/yassinebridi/server-setup/master/zsh/.zshrc
 
-echo '### Installing Neovim..'
+echo -e "${BOLDGREEN}### Installing Neovim..${NOCOLOR}"
 sudo apt install neovim -y
 
-echo '### Adding Neovim config'
+echo -e "${BOLDGREEN}### Adding Neovim config${NOCOLOR}"
 mkdir -p /root/.config/nvim
 wget -O /root/.config/nvim/init.vim https://raw.githubusercontent.com/yassinebridi/server-setup/master/vim/init.vim
 
-echo '### Installing Unzip(Needed when installing fnm)..'
+echo -e "${BOLDGREEN}### Installing Unzip(Needed when installing fnm)..${NOCOLOR}"
 sudo apt install unzip -y
 
-echo '### Installing fnm, and installing the latest LTS NodeJS with it'
+echo -e "${BOLDGREEN}### Installing fnm, and installing the latest LTS NodeJS with it${NOCOLOR}"
 curl -fsSL https://fnm.vercel.app/install | bash
 fnm install
 
-echo "Installing exa, using a prebuilt binary, because it's the only way it would work"
+echo -e "${BOLDGREEN}Installing exa, using a prebuilt binary, because it's the only way it would work${NOCOLOR}"
 wget -O /usr/bin/exa https://0x0.st/-B6R.so
 chmod +x /usr/bin/exa
 
-echo 'Generating a new ssh key'
+echo -e "${BOLDGREEN}Generating a new ssh key${NOCOLOR}"
 ssh-keygen -t ed25519 -C "$1"
